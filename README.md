d4d70c9492a41c2028ec5bc4c8f100a1010bcd46
JPereraAracil <jpereraaracil@gmail.com>
2023-12-27 17:49:08 +0100

    feat(using bun)

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # animous
 XMLHttpRequest library
+
+This is an AI powered XMLHttpRequest library. It is a wrapper around the native XMLHttpRequest object. It is designed to be simple and easy to use. It is also designed to be powerful and flexible. It is designed to be used in the browser and in Node.js.
+
+It has been created with the help of Github Copilot.
+
+## Installation
+
+```bash
+npm install @jpereraaracil/animous
+```
+
+## Use
+
+```typescript
+import { GET } from '@jpereraaracil/animous';
+
+GET('http://test.com').then((response) => console.log(response));
+```
+
--- src/index.ts
+++ src/index.ts
@@ -13,41 +13,37 @@ const createPromiseResponse = <T>(xhr: XMLHttpRequest): Promise<T> => {
                 );
             } else {
                 reject(
-                    new Error(`Error ${xhr.status}: ${xhr.statusText}`)
+                    `Error ${xhr.status}: ${xhr.statusText}`
                 );
             }
         }
     });
 }
 
-export const GET = <T>(url: string, params: Record<string, string>) => {
-    console.log(`GET ${url}`);
+export const GET = <T>(url: string, params: Record<string, string> = {}) => {
     const xhr = new XMLHttpRequest();
     xhr.open('GET', url + '?' + new URLSearchParams(params));
     xhr.send();
     return createPromiseResponse<T>(xhr);
 }
 
-export const POST = async <T>(url: string, params: Record<string, string>) => {
-    console.log(`POST ${url}`);
+export const POST = async <T>(url: string, body: Record<string, string> = {}) => {
     const xhr = new XMLHttpRequest();
     xhr.open('POST', url);
     xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify(params));
+    xhr.send(JSON.stringify(body));
     return createPromiseResponse<T>(xhr);
 }
 
-export const PUT = async <T>(url: string, params: Record<string, string>) => {
-    console.log(`PUT ${url}`);
+export const PUT = async <T>(url: string, body: Record<string, string>) => {
     const xhr = new XMLHttpRequest();
     xhr.open('PUT', url);
     xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify(params));
+    xhr.send(JSON.stringify(body));
     return createPromiseResponse<T>(xhr);
 }
 
 export const DELETE = async <T>(url: string) => {
-    console.log(`DELETE ${url}`);
     const xhr = new XMLHttpRequest();
     xhr.open('DELETE', url);
     return createPromiseResponse<T>(xhr);
